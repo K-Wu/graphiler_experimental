c370abae111869e3543d36047d77eb3948bf88de
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2022-12-12 03:57:18 -0600

    comment directive to disable jit opt

--- python/graphiler/update_all.py
+++ python/graphiler/update_all.py
@@ -1,5 +1,15 @@
+# import torch
 def update_all(g, mpdfg, msg_params=(), reduce_params=(), update_params=()):
-    updated = mpdfg.forward(g.DGLGraph, g.ndata, g.edata, g.ntype_data,
-                            g.etype_data, *msg_params, *reduce_params, *update_params)
+    # with torch.jit.optimized_execution(False):
+    updated = mpdfg.forward(
+        g.DGLGraph,
+        g.ndata,
+        g.edata,
+        g.ntype_data,
+        g.etype_data,
+        *msg_params,
+        *reduce_params,
+        *update_params
+    )
     for key in updated:
         g.ndata[key] = updated[key]
